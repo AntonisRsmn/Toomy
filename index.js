e9af13b7273e50fdc2fe406f43bde64fedfa1f92
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2022-07-17 08:01:48 +0000

    Change intents

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client, Collection, Intents } = require("discord.js");
 const { Token } = require("./config.json");
 
 // Intents
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: 641 });
 
 // Commands
 client.commands = new Collection();
